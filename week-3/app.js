61aa85992e67e82a3b65d0b66d0fbba2bd6debc0
jwu7778 <jwu7778@gmail.com>
2019-06-30 18:57:19 +0800

    week-3

---
+++ week-3/app.js
@@ -0,0 +1,49 @@
+var express=require('express');
+var cookie=require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var app=express();
+app.use(cookie());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var port=3000;
+app.listen(port);
+app.use(express.static('public'));
+
+app.get('/getData',function(req,res){
+    var query=req.query;
+    console.log(query.number);
+    var count=parseInt(query.number);
+    if(query.number==null){
+        return res.send ('Lack of Parameter');
+    }
+    if(isNaN(count)){
+        return res.send('Wrong Parameter');
+    }else{
+        var ans=(count+1)*count/2;
+        let result=ans;
+        return res.send(result + '');
+    }
+})
+
+app.get('/sum.html',function(req,res){
+    res.redirect('/sum.html');
+})
+
+app.get('/myName',function(req,res){
+    
+    if(req.cookies.name==null){
+        res.redirect('/trackName.html');
+    }else{
+        res.send(req.cookies.name)
+    }
+})
+
+app.get('/trackName',function(req,res){
+    console.log(req.cookies);
+    res.redirect('/myName');
+    
+})
+
